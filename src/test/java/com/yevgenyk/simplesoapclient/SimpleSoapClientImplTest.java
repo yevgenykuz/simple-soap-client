b3afe40633621bae247b952aff17b792a7f64382
Yevgeny Kuznetsov <yevgenyku@gmail.com>
2021-02-28 15:33:30 -0500

    Add SOAP server mocking

--- src/test/java/com/yevgenyk/simplesoapclient/SimpleSoapClientImplTest.java
+++ src/test/java/com/yevgenyk/simplesoapclient/SimpleSoapClientImplTest.java
@@ -1,5 +1,7 @@
 package com.yevgenyk.simplesoapclient;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
@@ -8,17 +10,43 @@ import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.io.IOException;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR;
+import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * These tests use a free SOAP service found @http://www.dneonline.com/calculator.asmx.
+ * These tests mock a free SOAP service found @http://www.dneonline.com/calculator.asmx.
  */
 class SimpleSoapClientImplTest {
+
+    private WireMockServer wireMockServer;
+
+    @AfterEach
+    void tearDown() {
+        if (wireMockServer != null) {
+            wireMockServer.stop();
+        }
+    }
 
     @Test
     void sendSoapRequest() throws IOException, SimpleSoapClientException, ParserConfigurationException, SAXException,
             XPathExpressionException, XmlParsingException {
-        SimpleSoapClient client = new SimpleSoapClientImpl("http://www.dneonline.com/calculator", "http://tempuri.org",
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        stubFor(post("/calculator.asmx?op=Add")
+                .willReturn(okTextXml(
+                        "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + System.lineSeparator() +
+                                "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+                                "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" + System.lineSeparator() +
+                                " <soap:Body>" + System.lineSeparator() +
+                                "  <AddResponse xmlns=\"http://tempuri.org/\">" + System.lineSeparator() +
+                                "   <AddResult>4</AddResult>" + System.lineSeparator() +
+                                "  </AddResponse>" + System.lineSeparator() +
+                                " </soap:Body>" + System.lineSeparator() +
+                                "</soap:Envelope>")));
+        SimpleSoapClient client = new SimpleSoapClientImpl("http://0.0.0.0:8080/calculator", "http://tempuri.org",
                 "Add");
         String response = client.sendSoapRequest(new File("src/test/resources/requestExample.xml"));
         String actual = XmlUtilities.getTextContentOfXmlElement(XmlUtilities.xmlStringToDocument(response),
@@ -28,12 +56,39 @@ class SimpleSoapClientImplTest {
 
     @Test
     void sendSoapRequestNegativeInternalServerError() throws SimpleSoapClientException {
-        SimpleSoapClient client = new SimpleSoapClientImpl("http://www.dneonline.com/calculator", "http://tempuri.org",
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        stubFor(post("/calculator.asmx?op=Add1")
+                .willReturn(aResponse()
+                        .withStatus(HTTP_INTERNAL_ERROR)
+                        .withHeader(CONTENT_TYPE, "text/xml; charset=utf-8")
+                        .withBody("<?xml version=\"1.0\" encoding=\"utf-8\"?>" + System.lineSeparator() +
+                                "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+                                "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                                "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" + System.lineSeparator() +
+                                " <soap:Body>" + System.lineSeparator() +
+                                "  <soap:Fault>" + System.lineSeparator() +
+                                "   <faultcode>" + System.lineSeparator() +
+                                "    soap:Client" + System.lineSeparator() +
+                                "   </faultcode>" + System.lineSeparator() +
+                                "   <faultstring>" + System.lineSeparator() +
+                                "    [truncated]System.Web.Services.Protocols.SoapException: " +
+                                "Server did not recognize the value of HTTP Header SOAPAction: " +
+                                "http://tempuri.org/Add1.\\r\\n   " +
+                                "at System.Web.Services.Protocols.Soap11ServerProtocolHelper.RouteRequest()\\r\\n   " +
+                                "at Syst" + System.lineSeparator() +
+                                "   </faultstring>" + System.lineSeparator() +
+                                "  <detail/>" + System.lineSeparator() +
+                                "  </soap:Fault>" +
+                                " </soap:Body>" + System.lineSeparator() +
+                                "</soap:Envelope>")
+                ));
+        SimpleSoapClient client = new SimpleSoapClientImpl("http://0.0.0.0:8080/calculator", "http://tempuri.org",
                 "Add1");
         SimpleSoapClientException simpleSoapClientException = assertThrows(SimpleSoapClientException.class,
                 () -> client.sendSoapRequest(new File("src/test/resources/requestExample.xml")));
         assertTrue(simpleSoapClientException.getMessage()
-                .contains("HTTP response was \"Internal Server Error\". Server returned:"));
+                .contains("HTTP response was \"Server Error\". Server returned:"));
     }
 
     @Test
